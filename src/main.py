c4af418d3cf910334020f16c7be65a036f27fddb
Ярослав Zhivem <mamayamertv@gmail.com>
2024-10-05 11:35:11 +0300

    v1.2

--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
 import sys
 import logging
+import ctypes
 from PyQt5 import QtWidgets
 from gui import GoodbyeDPIApp
 from utils import ensure_module_installed
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+
+    # Инициализация QApplication перед проверкой
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Проверка прав администратора
+    if not is_admin():
+        logging.error("Программа должна быть запущена с правами администратора.")
+        QtWidgets.QMessageBox.critical(None, "Ошибка", "Программа должна быть запущена с правами администратора.")
+        sys.exit(1)
 
     ensure_module_installed('packaging')
 
     try:
-        app = QtWidgets.QApplication(sys.argv)
         ex = GoodbyeDPIApp()
         ex.show()
         sys.exit(app.exec_())
