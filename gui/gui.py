49f28d0e4666d001f88280dd17ea2e7958c5a2a8
zhivem <mamayamertv@gmail.com>
2025-01-02 22:29:59 +0300

    2025

--- gui/gui.py
+++ gui/gui.py
@@ -627,7 +627,7 @@ class GoodbyeDPIApp(QtWidgets.QMainWindow):
             tr("Разработчик"): "Zhivem",
             tr("Репозиторий на GitHub"): f"<a href='https://github.com/zhivem/DPI-Penguin'>{tr('DPI Penguin')}</a>",
             tr("Релизы"): f"<a href='https://github.com/zhivem/DPI-Penguin/releases'>{tr('Все версии')}</a>",
-            tr("Лицензия"): tr("© 2024 Zhivem. Лицензия: Apache")
+            tr("Лицензия"): tr("© 2025 Zhivem. Лицензия: Apache")
         }
 
         widgets = {
